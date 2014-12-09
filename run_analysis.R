9f89047664d59543d753d5893143c9cccb7d7b19
Jan-Ove Karlberg <janove.karlberg@gmail.com>
2014-12-09 21:28:07 +0100

    Added code to produce the large tidy dataset.

--- run_analysis.R
+++ run_analysis.R
@@ -5,10 +5,13 @@ dataUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%2
 outputDirectory <- "output"
 
 ## Helper functions
-getMeanFeatures <- function() {
+getMeanAndStddevFeatures <- function() {
     features <- read.table("UCI HAR Dataset/features.txt", stringsAsFactors=FALSE)
     features <- data.table(FeatureNumber=features$V1, Description=features$V2)
     meanFeatures <- features[like(Description, "-mean()")]
+    stdFeatures <- features[like(Description, "-std()")]
+    
+    interestingFeatures <- rbind(meanFeatures, stdFeatures)
 }
 
 getActivites <- function() {
@@ -16,24 +19,79 @@ getActivites <- function() {
     activities <- data.table(ActivityNumber=activities$V1, Description=activities$V2)
 }
 
-installPackageIfMissing <- function(x) {
+#installPackageIfMissing <- function(x) {
+#    
+#    if (!require(x, character.only = TRUE)) {
+#        ## Install package if missing
+#        message(informationString)
+#        informationString = paste(c("Installing dependent package ", x))
+#        install.packages(x, dep=TRUE, repos="http://cran.rstudio.com/")
+#        
+#        ## Verify installation of package
+#        if (!require(x, character.only = TRUE)) {
+#            errorString <- paste(c("ERROR: Dependent package ", x, "could not be found"))
+#            stop(errorString)
+#        }
+#    }
+#}
+
+extractDesiredFeatures <- function(fileName, featureTable) {
+    rawFeatures <- read.table(fileName, stringsAsFactors=FALSE)
+    filteredFeatures  <- rawFeatures[, featureTable$FeatureNumber]
+}
+
+extractActivitiesByName <- function(activitiesFile, activityTable) {
+    rawActivities <- read.table(activitiesFile)
     
-    if (!require(x, character.only = TRUE)) {
-        ## Install package if missing
-        message(informationString)
-        informationString = paste(c("Installing dependent package ", x))
-        install.packages(x, dep=TRUE, repos="http://cran.rstudio.com/")
-        
-        ## Verify installation of package
-        if (!require(x, character.only = TRUE)) {
-            errorString <- paste(c("ERROR: Dependent package ", x, "could not be found"))
-            stop(errorString)
-        }
+    namedActivities <- vector()
+    for (activity in rawActivities$V1) {
+        namedActivities <- c(namedActivities, activityTable$Description[activity])
     }
+    
+    namedActivities
+}
+
+buildTidyDatasetPart <- function(isTest, activityTable, featureTable) {
+    
+    if (isTest) {
+        featuresFile <- "UCI HAR Dataset/test/X_test.txt"
+        subjectFile <- "UCI HAR Dataset/test/subject_test.txt"
+        activitiesFile <- "UCI HAR Dataset/test/Y_test.txt"
+        originLabel <- "Test"
+    } else {
+        featuresFile <- "UCI HAR Dataset/train/X_train.txt"
+        subjectFile <- "UCI HAR Dataset/train/subject_train.txt"
+        activitiesFile <- "UCI HAR Dataset/train/Y_train.txt"
+        originLabel <- "Training"
+    }
+    
+    ## Start by selecting the desired columns from the features file
+    filteredFeatures <- extractDesiredFeatures(featuresFile, featureTable)
+    
+    ## Extract the subjects
+    subjects = read.table(subjectFile)
+    
+    ## Extract the activities and map them to their names
+    activitiesByName <- extractActivitiesByName(activitiesFile, activityTable)
+    
+    ## Create a column representing the origin of the data
+    originColumn <- rep(originLabel, length(subjects$V1))
+    
+    ## Now combine all the columns into one table
+    tidyDatasetPart = data.table(Subject=subjects$V1, Origin=originColumn)
+    for (i in 1:length(featureTable$Description)) {
+        description <- featureTable$Description[i]
+        observations <- filteredFeatures[, i]
+        tidyDatasetPart[[description]] <- observations
+    }
+    tidyDatasetPart$Activity <- activitiesByName
+    #tidyDatasetPart[["Origin"]] <- originColumn
+    
+    tidyDatasetPart
 }
 
 ## Install dependent packages if not allready present
-requiredPackages <- c()
+#requiredPackages <- c()
 
 
 ## The project text states that the script should be able to run if the Samsung
@@ -53,9 +111,14 @@ if (!file.exists(outputDirectory)) {
 }
 
 ## Build a table of interesting features, i.e. those that include '-mean()'
-meanFeatures <- getMeanFeatures() 
-print(meanFeatures)
+interestingFeatures <- getMeanAndStddevFeatures() 
 
 ## Build a table of the activities
 activities <- getActivites()
-print(activities)
+
+## Build tidy versions of the training and test datasets
+tidyTraining <- buildTidyDatasetPart(FALSE, activities, interestingFeatures) 
+tidyTest <- buildTidyDatasetPart(TRUE, activities, interestingFeatures)
+
+## Combine the parts by "stacking them"
+tidyDataset <- rbind(tidyTraining, tidyTest)
